43ebcdaa24d2702a7ad0a975236a1b7251bb903b
ChenLin12138 <scorpion_chenlin@163.com>
2019-09-13 16:44:04 +0800

    Project Init

---
+++ src/main/java/com/lin/zuul/ZuulServiceApplication.java
@@ -0,0 +1,16 @@
+package com.lin.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+//使服务称为一个Zuul服务器
+@EnableZuulProxy
+public class ZuulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServiceApplication.class, args);
+	}
+
+}
